4af4ccfd407b00c2324605ade11f3a89929cfa85
victory <77779116+tmdrn9@users.noreply.github.com>
2021-05-12 16:46:53 +0900

    Binary Search Tree(BST)

--- Node.java
+++ Node.java
@@ -1,17 +1,18 @@
-
-public class Node<Key extends Comparable<Key>> {
-	private Key item;
-	private Node<Key> left;
-	private Node<Key> right;
-	public Node(Key newItem,Node ln,Node rn) {
-		item=newItem;
-		left=ln;
-		right=rn;
+public class Node <Key extends Comparable<Key>,Value> {
+	private Key id;
+	private Value name;
+	private Node left,right;
+	public Node(Key newId,Value newName) {
+		id=newId;
+		name=newName;
+		left=right=null;
 	}
-	public Key getKey() {return item;}
-	public Node<Key> getLeft(){return left;}
-	public Node<Key> getRight(){return right;}
-	public void setKey(Key newKey) {item=newKey;}
-	public void setLeft(Node<Key> ln) {left=ln;}
-	public void setRight(Node<Key> rn) {right=rn;}
+	public Key getKey() {return id;}
+	public Value getValue() {return name;}
+	public Node getLeft() {return left;}
+	public Node getRight() {return right;}
+	public void setKey(Key newId) {id=newId;}
+	public void setValue(Value newName) {name=newName;}
+	public void setLeft(Node newLeft) {left=newLeft;}
+	public void setRight(Node newRight) {right=newRight;}
 }
